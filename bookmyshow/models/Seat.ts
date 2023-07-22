eba7e68592a88402dcedab739c8592d6fda5b325
Paras Agrawal <parasagrawal71@gmail.com>
2023-07-22 18:27:27 +0530

    Change

--- bookmyshow/models/Seat.ts
+++ bookmyshow/models/Seat.ts
@@ -1,11 +1,11 @@
 class Seat {
   private _id: string;
-  private row: string;
+  private _row: string;
   private _seatNo: number;
 
   constructor(id: string, row: string, seatNo: number) {
     this._id = id;
-    this.row = row;
+    this._row = row;
     this._seatNo = seatNo;
   }
 
@@ -14,7 +14,7 @@ class Seat {
   }
 
   getRow(): string {
-    return this.row;
+    return this._row;
   }
 
   getSeatNo(): number {
