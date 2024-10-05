3cc3873188272e6bbdc3bc7ca9cbf4aba41e4300
Paras Agrawal <parasagrawal71@gmail.com>
2024-10-06 00:11:30 +0530

    Modify decorator pattern

--- _desgin-patterns/3. Decorator Design Pattern/Main.ts
+++ _desgin-patterns/3. Decorator Design Pattern/Main.ts
@@ -1,8 +1,12 @@
 import { BasePizza } from "./Pizza/BasePizza";
 import { FarmHouse } from "./Pizza/FarmHouse";
 import { ExtraCheese } from "./Toppings/ExtraCheese";
+import { Mushroom } from "./Toppings/Mushroom";
 
 (function main() {
   const pizza1: BasePizza = new ExtraCheese(new FarmHouse());
   console.log(pizza1.cost());
+
+  const pizza2: BasePizza = new Mushroom(new ExtraCheese(new FarmHouse()));
+  console.log(pizza2.cost());
 })();
